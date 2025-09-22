aae59e426de747abf774dc619a3e431f1664e440
gml18 <gemelasleiva@gmail.com>
2025-09-22 01:53:50 -0600

    img

--- src/data/topics.js
+++ src/data/topics.js
@@ -4,31 +4,31 @@
 
 export const temas = {
   animales: [
-    { word: 'Dog',  img: '${base}img/animals/dog.jpeg',  audio: '/audio/animals/dog.mp3' },
-    { word: 'Cat',  img: '${base}img/animals/cat.jpeg',  audio: '/audio/animals/cat.mp3' },
-    { word: 'Duck', img: '${base}img/animals/duck.jpeg' },
-    { word: 'Frog', img: '${base}img/animals/frog.jpeg' },
+    { word: 'Dog',  img: '/img/animals/dog.jpeg',  audio: '/audio/animals/dog.mp3' },
+    { word: 'Cat',  img: '/img/animals/cat.jpeg',  audio: '/audio/animals/cat.mp3' },
+    { word: 'Duck', img: '/img/animals/duck.jpeg' },
+    { word: 'Frog', img: '/img/animals/frog.jpeg' },
   ],
 
   colores: [
-    { word: 'Red',    img: '${base}img/colors/red.jpeg' },
-    { word: 'Blue',   img: '${base}img/colors/blue.jpeg' },
-    { word: 'Green',  img: '${base}img/colors/green.jpeg' },
-    { word: 'Yellow', img: '${base}img/colors/yellow.jpeg' },
+    { word: 'Red',    img: '/img/colors/red.jpeg' },
+    { word: 'Blue',   img: '/img/colors/blue.jpeg' },
+    { word: 'Green',  img: '/img/colors/green.jpeg' },
+    { word: 'Yellow', img: '/img/colors/yellow.jpeg' },
   ],
 
   numeros: [
-    { word: 'One',   img: '${base}img/numbers/one.jpeg' },
-    { word: 'Two',   img: '${base}img/numbers/two.jpeg' },
-    { word: 'Three', img: '${base}img/numbers/three.jpeg' },
-    { word: 'Four',  img: '${base}img/numbers/four.jpeg' },
+    { word: 'One',   img: '/img/numbers/one.jpeg' },
+    { word: 'Two',   img: '/img/numbers/two.jpeg' },
+    { word: 'Three', img: '/img/numbers/three.jpeg' },
+    { word: 'Four',  img: '/img/numbers/four.jpeg' },
   ],
 
-   alfabeto: [
-    { word: 'A', img: '${base}img/alphabets/a.jpeg' },
-    { word: 'B', img: '${base}img/alphabets/b.jpeg' },
-    { word: 'C', img: '${base}img/alphabets/c.jpeg' },
-    { word: 'D', img: '${base}img/alphabets/d.jpeg' },
+  alfabeto: [
+    { word: 'A', img: '/img/alphabets/a.jpeg' },
+    { word: 'B', img: '/img/alphabets/b.jpeg' },
+    { word: 'C', img: '/img/alphabets/c.jpeg' },
+    { word: 'D', img: '/img/alphabets/d.jpeg' },
   ],
 };
 
@@ -39,5 +39,5 @@ export function speak(text) {
     u.lang = 'en-US';
     window.speechSynthesis.cancel();
     window.speechSynthesis.speak(u);
-  } catch (_) {}
+} catch (_) {}
 }
